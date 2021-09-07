0887f9a1fa1d7a1acf47d0633a07fa0e3942eef0
LonelyDragon <diagnozzz123@gmail.com>
2021-09-07 15:24:52 +0300

    remove env user setting

--- conftest.py
+++ conftest.py
@@ -15,7 +15,7 @@ def browser(request):
     options.add_experimental_option(
         'prefs', {'intl.accept_languages': user_language})
     print("\nstart chrome browser for test..")
-    browser = webdriver.Chrome("./chromedriver", options=options)
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
