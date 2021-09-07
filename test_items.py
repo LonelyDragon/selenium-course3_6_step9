2f79c5d6bb568ba28428f7465412e04e69f4cc35
Anton Zavodchikov <diagnozzz123@gmail.com>
2021-09-07 15:19:12 +0300

    add sol from 3.6

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_button_lang(browser):
+    browser.get(url)
+    button = browser.find_elements_by_class_name(
+        "btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(button) > 0, "Button is not defined"
+    time.sleep(5)
